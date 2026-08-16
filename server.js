9fb8c3392147482e1ca107ae7ba0eed4e487c00c
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-08-16 07:12:41 +0500

    data base connecting

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
+
+// Database
+connectDB();
+
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+
+// Test route
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -14,8 +26,11 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT =  5000;
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+  console.log(
+    `Backend running on http://localhost:${PORT}`
+  );
 });
