be8f5950d3c91520c9f82b28a6407d8a5ac2a8dd
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-08-21 06:58:54 +0500

    server cors changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://YOUR-FRONTEND.vercel.app",
+      "https://full-stack-assignment.vercel.app",
     ],
     credentials: true,
   })
